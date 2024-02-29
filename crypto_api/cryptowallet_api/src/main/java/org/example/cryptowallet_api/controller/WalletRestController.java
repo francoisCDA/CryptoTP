c85917b7ec3664e5ba4ddbced8d694a9fdca68b9
loic <loic.gstrn@gmail.com>
2024-02-29 11:03:53 +0100

    Wallet Api create transaction / get wallet

--- crypto_api/cryptowallet_api/src/main/java/org/example/cryptowallet_api/controller/WalletRestController.java
+++ crypto_api/cryptowallet_api/src/main/java/org/example/cryptowallet_api/controller/WalletRestController.java
@@ -1,9 +1,11 @@
 package org.example.cryptowallet_api.controller;
 
+import org.example.cryptowallet_api.dto.TransactionDTO;
 import org.example.cryptowallet_api.service.WalletService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/wallet")
@@ -14,5 +16,19 @@ public class WalletRestController {
     public WalletRestController(WalletService walletService) {
         this.walletService = walletService;
     }
-    
+
+
+    @GetMapping("{idPerson}/{cryptoCurrencyName}") // http://localhost:8098/api/wallet/{idPerson}/{cryptoCurrencyName}
+    public Flux<TransactionDTO> getAll(@PathVariable("idPerson") UUID idPerson, @PathVariable("cryptoCurrencyName") String cryptoCurrencyName) {
+        return walletService.getCryptoWalletByPersonId(idPerson, cryptoCurrencyName);
+    }
+
+    @PostMapping("post") // http://localhost:8083/api/wallet
+    public void postTransaction(@RequestBody TransactionDTO transactionDTO){
+        walletService.createNewTransaction(String.valueOf(transactionDTO.getTransactionDate()),
+                        transactionDTO.getCryptoCurrencyName(),
+                        transactionDTO.getQuantity(),
+                        transactionDTO.getPrice(), UUID.randomUUID());
+    }
+
 }
